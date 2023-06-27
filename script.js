a7b8463c2a9f5d7fd41196ad7e0c6083772124f0
DonBenito <pcl.ben.6@gmail.com>
2023-06-27 15:58:20 +0200

    Réorganisation du code pour faciliter la lisibilité

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 console.log('BlackJack')
 
-// Initialisation des paramètres nécéssaires pour jouer
+// Initialisation des paramètres nécessaires pour jouer
+// - Crédit restant
 let userCredits = 1000
 console.log("Nous vous offrons 1000 crédits")
-
-// Initialisation du paquet de carte vide
+// - Crédits à miser
+let amountBet= 0
+// - Initialisation du paquet de carte vide
 const elsCards = []
 // const elsUserCards = []
 // const elsDealerCards = []
+
+
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -17,150 +21,15 @@ const elsCards = []
 const body = document.body;
 body.innerHTML = ''
 
-// Création de la structure de la page
-const header = createBalise("header", body, "flexColumn")
-const main = createBalise("main", body, "flexCenter")
-const footer = createBalise("footer", body, "flexCenter")
-footer.style.color = "var(--button-color)"
 
-// --- Création de la structure à l'intérieur du header
-// Création du titre
-setTitle("BlackJack", header);
+// Création de la structure principale de la page
+createDOM()
 
-// Création de la zone "informations"
-const elsInformations = createBalise("div", header, "flexRow");
-elsInformations.id = "infos";
-
-// Affichage des crédits restants
-const elCredit = createBalise("div", elsInformations, "credit-info");
-elCredit.textContent = userCredits.toLocaleString();
-
-// Affichage du bouton d'ouverture de la modale paramètres
-const elParams = createBalise("div", elsInformations, "params");
-const paramBtn = createBalise("button", elParams);
-paramBtn.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="icon" height="1em" viewBox="0 0 512 512"><path d="M495.9 166.6c3.2 8.7 .5 18.4-6.4 24.6l-43.3 39.4c1.1 8.3 1.7 16.8 1.7 25.4s-.6 17.1-1.7 25.4l43.3 39.4c6.9 6.2 9.6 15.9 6.4 24.6c-4.4 11.9-9.7 23.3-15.8 34.3l-4.7 8.1c-6.6 11-14 21.4-22.1 31.2c-5.9 7.2-15.7 9.6-24.5 6.8l-55.7-17.7c-13.4 10.3-28.2 18.9-44 25.4l-12.5 57.1c-2 9.1-9 16.3-18.2 17.8c-13.8 2.3-28 3.5-42.5 3.5s-28.7-1.2-42.5-3.5c-9.2-1.5-16.2-8.7-18.2-17.8l-12.5-57.1c-15.8-6.5-30.6-15.1-44-25.4L83.1 425.9c-8.8 2.8-18.6 .3-24.5-6.8c-8.1-9.8-15.5-20.2-22.1-31.2l-4.7-8.1c-6.1-11-11.4-22.4-15.8-34.3c-3.2-8.7-.5-18.4 6.4-24.6l43.3-39.4C64.6 273.1 64 264.6 64 256s.6-17.1 1.7-25.4L22.4 191.2c-6.9-6.2-9.6-15.9-6.4-24.6c4.4-11.9 9.7-23.3 15.8-34.3l4.7-8.1c6.6-11 14-21.4 22.1-31.2c5.9-7.2 15.7-9.6 24.5-6.8l55.7 17.7c13.4-10.3 28.2-18.9 44-25.4l12.5-57.1c2-9.1 9-16.3 18.2-17.8C227.3 1.2 241.5 0 256 0s28.7 1.2 42.5 3.5c9.2 1.5 16.2 8.7 18.2 17.8l12.5 57.1c15.8 6.5 30.6 15.1 44 25.4l55.7-17.7c8.8-2.8 18.6-.3 24.5 6.8c8.1 9.8 15.5 20.2 22.1 31.2l4.7 8.1c6.1 11 11.4 22.4 15.8 34.3zM256 336a80 80 0 1 0 0-160 80 80 0 1 0 0 160z"/></svg>`;
-paramBtn.addEventListener("click", handlerDispParams);
-
-// --- Création d'un bouton pour démarrer le jeu
-const playBtn = createButton('Jouer', main);
-playBtn.addEventListener("click", showTokens);
-
-// --- Création d'un bouton pour démarrer le jeu
-const dealBtn = document.createElement('button');
-dealBtn.type = "button"
-dealBtn.value = "Miser"
-dealBtn.textContent = "Miser"
-dealBtn.addEventListener("click", startGame);
 
 // Initialisation de la modale incluant les paramètres
-const elModalPanel = document.createElement("div"),
-    elModalZone = document.createElement("div"),
-    elModalTitle = document.createElement("div"),
-    elModalBody = document.createElement("div"),
-    elModalFooter = document.createElement("div"),
-    elModalCancelBtn = document.createElement("button"),
-    elModalConfirmBtn = document.createElement("button");
-
-    elModalPanel.classList.add("modal-panel")
-    elModalPanel.classList.add("hidden")
-
-    elModalZone.classList.add("modal")
-    elModalZone.classList.add("flexColumnWithSpace")
-
-    elModalTitle.classList.add("modal-title")
-    elModalTitle.classList.add("flexCenter")
-    elModalTitle.textContent = "Paramètres"
-
-    elModalBody.classList.add("modal-body")
-    elModalBody.classList.add("param-color")
-    elModalBody.classList.add("flexCenter")
-
-    elModalFooter.classList.add("modal-footer")
-
-    elModalCancelBtn.textContent = "Annuler"
-    elModalCancelBtn.dataRole = "cancel"
-    elModalConfirmBtn.textContent = "Valider"
-    elModalConfirmBtn.dataRole = "confirm"
-
-    body.appendChild(elModalPanel)
-    elModalPanel.appendChild(elModalZone)
-    elModalZone.append(elModalTitle, elModalBody, elModalFooter)
-    elModalFooter.append(elModalCancelBtn, elModalConfirmBtn)
-
-// Collection d'affichage
-    const cardParamCollection = [
-        {
-            text: "blue",
-            el: document.createElement("input"),
-            class: "blueBase"
-        },
-        {
-            text: "red",
-            el: document.createElement("input"),
-            class: "redBase"
-        },
-        {
-            text: "purple",
-            el: document.createElement("input"),
-            class: "purpleBase"
-        },
-        {
-            text: "teal",
-            el: document.createElement("input"),
-            class: "tealBase"
-        },
-        {
-            text: "orange",
-            el: document.createElement("input"),
-            class: "orangeBase"
-        },
-    ]
-
-    const elsCardParamListBtn = []
-
-    const dataRole = document.createAttribute("data-role")
-    for (let card of cardParamCollection) {
-        const choiceColor = document.createElement("input");
-        choiceColor.type = "button"
-
-        choiceColor.dataRole = card.text
-        choiceColor.value = card.text
-        choiceColor.classList.add("card")
-        choiceColor.color = "text"
-        // choiceColor.textContent = card.text
-        choiceColor.classList.add(card.class)
-        elModalBody.appendChild(choiceColor)
-        elsCardParamListBtn.push(choiceColor)
-    }
-
-    elsCardParamListBtn[1].classList.add("activatedColorParam")
-    let activatedParam = elsCardParamListBtn[1]
-    let actualColorChoice = activatedParam.dataRole + "Base"
-    console.log(actualColorChoice)
-
-    elModalPanel.addEventListener("click", function (evt) {
-        console.log(evt.target.dataRole)
-        if (evt.target.dataRole === "confirm") {
-            setParam(actualColorChoice)
-            elModalPanel.classList.add("hidden")
-        } else if (evt.target.dataRole === "cancel") {
-            elModalPanel.classList.add("hidden")
-        } else if (evt.target.dataRole === "undefined") {
-
-        } else if (activatedParam.dataRole !== evt.target.dataRole) {
-
-            activatedParam.classList.remove("activatedColorParam")
-            activatedParam = evt.target
-            activatedParam.classList.add("activatedColorParam")
-
-            actualColorChoice = activatedParam.dataRole + "Base"
-            console.log(actualColorChoice)
-        } else {
-
-        }
+createModale()
 
 
-    })
 
     // Préparation de la div incluant les jetons pour miser
     const elCoins = document.createElement('div');
@@ -169,6 +38,8 @@ const elModalPanel = document.createElement("div"),
     const elValues = [[5, '#FFFFFF'],[10, '#0000FF'],[25, '#FF0000'],[50, '#008000'],[100, '#000000']]
 
     for(let i = 0; i < 5; i++) {
+        // création du jeton
+
         // création de la couleur de fond
         const elCoinBG = document.createElement('div')
         elCoins.appendChild(elCoinBG);
@@ -182,6 +53,20 @@ const elModalPanel = document.createElement("div"),
         if(i === 0) {elCoin.style.color = 'black'}
         elCoin.textContent = elValues[i][0];
         elCoinBG.appendChild(elCoin);
+
+        // association de l'événement au click
+        elCoin.addEventListener('click', function (evt) {
+
+            // si l'utilisateur a assez de crédit, on incrémente le montant à miser
+            if((amountBet + parseInt(evt.target.textContent)) <= userCredits) {
+                amountBet += parseInt(evt.target.textContent)
+
+                // on met à jour l'affichage sur le bouton
+                elDealBtn.textContent = `Miser ${amountBet} crédits`
+            }
+
+
+        })
 }
 
     body.appendChild(elCoins);
@@ -201,7 +86,7 @@ function showTokens() {
 // Affichage des crédits restants
 function dispCredit() {
     elCoins.classList.remove("out")
-    main.appendChild(dealBtn)
+    main.appendChild(elDealBtn)
 }
 
 // Gestion du bouton d'affichage de la modale de paramètres
@@ -212,6 +97,7 @@ function handlerDispParams() {
 function startGame()
 {
     elCoins.classList.add("out")
+    elDealBtn.remove()
 
 // --- Création de la structure à l'intérieur du body
 // Création de la zone de jeu
---
+++ tools.js
@@ -0,0 +1,63 @@
+
+// Fonction : 
+// Création d'une balise <type> dans parent
+function createBalise(type, parent, classe = "") {
+    const myBalise = document.createElement(type)
+    if (classe != "") myBalise.classList.add(classe)
+    parent.appendChild(myBalise)
+    return myBalise
+}
+
+// Création d'un button dans parent
+function createButton(value, parent, classe = "") {
+    const myBalise = document.createElement("input")
+    myBalise.type = "button"
+    myBalise.value = value
+    if (classe != "") myBalise.classList.add(classe)
+    parent.appendChild(myBalise)
+    return myBalise
+}
+
+// Création de "n" balises <type> dans parent
+function createMultipleBalise(type, n, parent, classe = "") {
+    const Balises = []
+    for (let i = 0; i < n; i++) {
+        const myBalise = document.createElement(type)
+        if (classe != "") myBalise.classList.add(classe)
+        parent.appendChild(myBalise)
+        Balises.push(myBalise)
+    }
+    return Balises
+}
+
+// Fonction :
+// Ajout du titre h1 dans une balise
+function setTitle(title, parent) {
+    const titleDiv = document.createElement("h1")
+    titleDiv.textContent = title
+    parent.appendChild(titleDiv)
+}
+
+function setParam(colorBase) {
+    for(let card of elsCards) {
+        console.log("removeBase")
+        card.classList.remove("blueBase")
+        card.classList.remove("redBase")
+        card.classList.remove("purpleBase")
+        card.classList.remove("tealBase")
+        card.classList.remove("orangeBase")
+        card.classList.add(colorBase)
+    }
+}
+
+function cancelParam(colorBase) {
+    for(let card of elsCards) {
+        console.log("removeBase")
+        card.classList.remove("blueBase")
+        card.classList.remove("redBase")
+        card.classList.remove("purpleBase")
+        card.classList.remove("tealBase")
+        card.classList.remove("orangeBase")
+        card.classList.add(colorBase)
+    }
+}
